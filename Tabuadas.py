eca8cf3851ea25f008145878cf4f652df6056659
Jose Rodrigues <99560762+Josefilhotec@users.noreply.github.com>
2022-02-21 08:54:31 -0300

    Update Tabuadas.py

--- Tabuadas.py
+++ Tabuadas.py
@@ -11,7 +11,7 @@ for c in range(1, 11):
 print('TABUADA DE ADIÇÃO:')
 num = int(input('Digite o numero: '))
 for c in range(1, 11):
-    print(' {:2}  +  {}  =  {:2}'.format(c, num, num + c))
+    print(' {:2}  +  {}  =  {:2}'.format(num, c, num + c))
 
 print('TABUADA DE SUBTRAÇÃO')
 num = int(input('Digite o numero: '))
